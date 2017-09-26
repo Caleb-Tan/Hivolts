741a15214e118a6e83e579b3ad698d220957659a
Caleb-Tan <calebtan23@gmail.com>
2017-09-26 09:45:12 -0700

    started making grid

--- Hivolts/src/Grid.java
+++ Hivolts/src/Grid.java
@@ -1,4 +1,11 @@
+import java.awt.*;
+import javax.swing.JPanel;
 
-public class Grid {
+public class Grid extends JPanel {
+    Grid(){
+        // sets preferred size of window
+        setPreferredSize(new Dimension(750,750));
+    };
 
+    private void paint()
 }
