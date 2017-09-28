d20c60c3741517525383e4b43b5b760298177d13
BYTurnips <brionqiye@gmail.com>
2017-09-28 09:15:36 -0700

    changed grid to game

--- Hivolts/src/main.java
+++ Hivolts/src/main.java
@@ -5,10 +5,10 @@ import javax.swing.*;
  */
 public class Main {
     public static void main(String[] args){
-        Grid grid = new Grid();				// creates grid object
+        Game game = new Game();				// creates grid object
         JFrame frame = new JFrame("Hivolts");	// creates jframe
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(grid);	// adds the flag panel to the jframe
+        frame.add(game);	// adds the flag panel to the jframe
         frame.pack(); // sizes frame so that all contents are at preferred sizes
         frame.setVisible(true);
     }
