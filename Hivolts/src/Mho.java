2759de4af6abea92c8a0f2bbe9c905457bfad9f2
BYTurnips <brionqiye@gmail.com>
2017-09-28 08:59:08 -0700

    deleted fence file#

--- Hivolts/src/Mho.java
+++ Hivolts/src/Mho.java
@@ -1,4 +1,17 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-public class Mho {
-
+public class Mho extends JPanel {
+	int xcor, ycor;
+	Image fence;
+    Mho(int x, int y) {
+        ImageIcon fenceIcon = new ImageIcon("../res/fence.jpg");
+        fence = fenceIcon.getImage();
+        xcor = x;
+        ycor = y;
+    }
+    public void paint(Graphics g) {
+        g.drawImage(fence, 0, 0, this);
+    }
 }
