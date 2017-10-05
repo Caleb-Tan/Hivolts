d2e6fb021900ddf5b4ea71c0abc4a32b9d128e58
BYTurnips <brionqiye@gmail.com>
2017-10-05 09:14:22 -0700

    started mho movement

--- Hivolts/src/Game.java
+++ Hivolts/src/Game.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 public class Game extends JPanel implements KeyListener {
 
     private Cell cell = new Cell();                             // grid coordinates
-    private Fence fencePerimeter = new Fence();                 // object used solely to paint the perimeter of the fence
+    private Fence fencePerimeter = new Fence();  // object used solely to paint the perimeter of the fence
     private Player player;                                  // declares player object to use later
     private ArrayList<Fence> fences = new ArrayList<Fence>();   // contains all the fences
     private ArrayList<Mho> mhos = new ArrayList<Mho>();         // contains all the mhos
@@ -18,21 +18,21 @@ public class Game extends JPanel implements KeyListener {
         return new Dimension(720,720);
     }
 
-
     public void paint(Graphics g){
         super.paint(g);
         Color green = new Color(10,130,0);
         g.setColor(green);
-        g.fillRect(0,0,720,720);       // fills background rectangle to be green
-        fencePerimeter.paintFencePerimeter(g);             // paints fence perimeter (see method in Fence.java)
-        generateElements(g);                               // calls method to generate elements
+        g.fillRect(0,0,720,720); // fills background rectangle to be green
+        fencePerimeter.paintFencePerimeter(g);  // paints fence perimeter (see method in Fence.java)
+        generateElements(g);   // calls method to generate elements
     }
 
     /* method that randomly generates the initial positions, */
     private void generateElements(Graphics g) {
-        ArrayList<Integer> coords = cell.getGridCoords();                   // get the grid coords
-        ArrayList<ArrayList<Integer>> shuffledCoords = new ArrayList<>();   // makes the arraylist that contains arraylists to represent each cell coord
-        coords.remove(0);       // removes 70 and 770 so that only inside cells are considered
+        ArrayList<Integer> coords = cell.getGridCoords();   // get the grid coords
+     // makes the arraylist that contains arraylists to represent each cell coord
+        ArrayList<ArrayList<Integer>> shuffledCoords = new ArrayList<>();
+        coords.remove(0);  // removes 70 and 770 so that only inside cells are considered
         coords.remove(10);
 
         for (int i = 0; i <= 9; i++) {
@@ -47,11 +47,14 @@ public class Game extends JPanel implements KeyListener {
         Collections.shuffle(shuffledCoords);     // shuffles the collection with each cell coord in it
         for (int i = 0; i <= 32; i++) {
             if (i <= 11) {
-                mhos.add(new Mho(g, shuffledCoords.get(i).get(0), shuffledCoords.get(i).get(1)));   // first 12 of the shuffled coords are mhos
+            	// first 12 of the shuffled coords are mhos
+                mhos.add(new Mho(g, shuffledCoords.get(i).get(0), shuffledCoords.get(i).get(1)));
             } else if (i <= 31){
-                fences.add(new Fence(g, shuffledCoords.get(i).get(0), shuffledCoords.get(i).get(1)));   // next 20 of the shuffled coords are fences
+            	// next 20 of the shuffled coords are fences
+                fences.add(new Fence(g, shuffledCoords.get(i).get(0), shuffledCoords.get(i).get(1)));
             } else {
-                player = new Player(g, shuffledCoords.get(32).get(0), shuffledCoords.get(32).get(1));           // 33rd coordinate is player
+            	// 33rd coordinate is player
+                player = new Player(g, shuffledCoords.get(32).get(0), shuffledCoords.get(32).get(1));
             }
         }
     }
