e5617e1868dc0f243a0de98f077a044f27d011a5
Caleb-Tan <calebtan23@gmail.com>
2017-10-10 09:03:23 -0700

    added gameOver method

--- Hivolts/src/Mho.java
+++ Hivolts/src/Mho.java
@@ -5,49 +5,47 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Mho extends Element {
-	private BufferedImage mho;
-	/* coordinates for the mho object and creating the image */
-	public Mho (int x, int y) {
-		try {
+    private BufferedImage mho;
+
+    /* coordinates for the mho object and creating the image */
+    public Mho(int x, int y) {
+        try {
             mho = ImageIO.read(new File(ImagePath.mhoPath));
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         this.x = x;
         this.y = y;
-	}
-	
-	public void paintMho(Graphics g) {
-		g.drawImage(mho, x, y, null);
     }
 
-	/* method causes mhos to move towards player */
-	public void moveTowards(int px, int py) {
-		int rightOf, above;
-		
-		if (x < px) rightOf = 1;
-		else if (x == px) rightOf = 0;
-		else rightOf = -1;
-		if (y < py) above = 1;
-		else if (y == py) above = 0;
-		else above = -1;
-		
-		int farx = (Math.abs((px - x)) > Math.abs((py - y))) ? 1 : 0;
-		
-	    	if ((rightOf == 0)||(above == 0)) {
-			move(rightOf, above);
-		}
-		else {
-			if (Game.isEmpty(x-60*rightOf, y-60*above)) {
-				System.out.println("it's empty yay: " + rightOf + " " + above);
-				move(rightOf, above);
-			}
-			else if (Game.isEmpty(x+60*rightOf*farx, y+60*above*(1-farx))) {
-				move(rightOf*farx, above*(1-farx));
-			}
-			else if (Game.isEmpty(x+60*rightOf*(1-farx), y+60*above*farx)) {
-				move(rightOf*(1-farx), above*farx);
-			}
-		}
+    public void paintMho(Graphics g) {
+        g.drawImage(mho, x, y, null);
+    }
+
+    /* method causes mhos to move towards player */
+    public void moveTowards(int px, int py) {
+        int rightOf, above;
+
+        if (x < px) rightOf = 1;
+        else if (x == px) rightOf = 0;
+        else rightOf = -1;
+        if (y < py) above = 1;
+        else if (y == py) above = 0;
+        else above = -1;
+
+        int farx = (Math.abs((px - x)) > Math.abs((py - y))) ? 1 : 0;
+
+        if ((rightOf == 0) || (above == 0)) {
+            move(rightOf, above);
+        } else {
+            if (Game.isEmpty(x - 60 * rightOf, y - 60 * above)) {
+                System.out.println("it's empty: " + rightOf + " " + above);
+                move(rightOf, above);
+            } else if (Game.isEmpty(x + 60 * rightOf * farx, y + 60 * above * (1 - farx))) {
+                move(rightOf * farx, above * (1 - farx));
+            } else if (Game.isEmpty(x + 60 * rightOf * (1 - farx), y + 60 * above * farx)) {
+                move(rightOf * (1 - farx), above * farx);
+            }
+        }
     }
 }
--- Hivolts/src/Player.java
+++ Hivolts/src/Player.java
@@ -5,13 +5,13 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Player extends Element{
+public class Player extends Element {
     private BufferedImage player;
 
-    Player (int x, int y){
+    Player(int x, int y) {
         try {
             player = ImageIO.read(new File(ImagePath.playerPath));
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         this.x = x;
@@ -21,48 +21,48 @@ public class Player extends Element{
     protected void paintPlayer(Graphics g) {
         g.drawImage(player, x, y, null);
     }
-    
+
     protected void move(int key) {
-    	switch (key){
-        case KeyEvent.VK_W: {
-            move(0, -1);
-            break;
+        switch (key) {
+            case KeyEvent.VK_W: {
+                move(0, -1);
+                break;
+            }
+            case KeyEvent.VK_X: {
+                move(0, 1);
+                break;
+            }
+            case KeyEvent.VK_A: {
+                move(-1, 0);
+                break;
+            }
+            case KeyEvent.VK_D: {
+                move(1, 0);
+                break;
+            }
+            case KeyEvent.VK_Q: {
+                move(-1, -1);
+                break;
+            }
+            case KeyEvent.VK_E: {
+                move(1, -1);
+                break;
+            }
+            case KeyEvent.VK_Z: {
+                move(-1, 1);
+                break;
+            }
+            case KeyEvent.VK_C: {
+                move(1, 1);
+                break;
+            }
+            case KeyEvent.VK_S: {
+                move(0, 0);
+                break;
+            }
+            default: {
+                return;
+            }
         }
-        case KeyEvent.VK_X: {
-            move(0, 1);
-            break;
-        }
-        case KeyEvent.VK_A: {
-            move(-1, 0);
-            break;
-        }
-        case KeyEvent.VK_D: {
-            move(1, 0);
-            break;
-        }
-        case KeyEvent.VK_Q: {
-            move(-1, -1);
-            break;
-        }
-        case KeyEvent.VK_E: {
-            move(1, -1);
-            break;
-        }
-        case KeyEvent.VK_Z: {
-            move(-1, 1);
-            break;
-        }
-        case KeyEvent.VK_C: {
-        		move(1, 1);
-        		break;
-        }
-        case KeyEvent.VK_S: {
-        		move(0, 0);
-        		break;
-        }
-        default: {
-        		return;
-        }
-    }
     }
 }
